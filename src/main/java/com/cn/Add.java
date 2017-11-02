a5f43887364b379c5d3fd04015e758a737c7effc
Administrator <Administrator@MS-20160625LRRB>
2017-11-03 01:40:34 +0800

    测试01

---
+++ src/main/java/com/cn/Add.java
@@ -0,0 +1,9 @@
+package com.cn;
+
+public class Add {
+	public int add(int x,int y)
+	{
+		return x+y;
+	}
+
+}
---
+++ src/test/java/com/cn/AddTest.java
@@ -0,0 +1,25 @@
+package com.cn;
+/*
+ * testNG的参数化案例
+ */
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class AddTest {
+
+  @DataProvider(name="c")
+  public Object[][] createDate()               
+	{
+		return new Object[][]{
+				{4,2,2},{2,1,3},{9,3,12}
+				};
+	}
+	
+  @Test(dataProvider="c")
+  public void add(int x,int y,int result) 
+  {
+	Add a=new Add();
+    Assert.assertEquals(a.add(x, y), result);
+  }
+}
